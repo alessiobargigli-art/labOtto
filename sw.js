c28b7bfb663d2ec76b71930193d4a6cf61c0699c
alessiobargigli-art <alessio.bargigli@gmail.com>
2026-09-13 17:58:46 +0200

    chore: cache LAB-8 audio module

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'lab8-v0.4.1';
+const CACHE_NAME = 'lab8-v0.4.2';
 const APP_SHELL = [
   './',
   './index.html',
   './styles.css',
   './game.js',
+  './audio.js',
   './manifest.webmanifest',
   './icons/icon-192.png',
   './icons/icon-512.png',
