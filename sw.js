254ed4f2e704ca6b66a1a1708dc2ab6d9b371cfc
alessiobargigli-art <alessio.bargigli@gmail.com>
2026-09-14 14:58:39 +0200

    release: cache LAB-8 2.0.1 spawn tuning

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lab8-v2.0.0';
+const CACHE_NAME = 'lab8-v2.0.1';
 const APP_SHELL = [
   './',
   './index.html',
@@ -7,6 +7,7 @@ const APP_SHELL = [
   './levels.js',
   './campaign-core.js',
   './game-v2.js',
+  './spawn-tuning.js',
   './audio.js',
   './pin.js',
   './settings.js',
